ef3abe1474ee8ce805b4a0564dac5968dde593d0
Dallas Hall <dallas.hall@gmail.com>
2018-04-02 22:55:28 -0700

    Add ability to track users online status

--- index.js
+++ index.js
@@ -29,34 +29,36 @@ app.get('/', (req, res) => res.send('Hello!!'));
 //     * Upon success, emit action to clients
 
 io.on('connection', (socket) => {
-  const { room, user } = socket.handshake.query;
+  const { room, user } = JSON.parse(socket.handshake.query.payload);
+  console.log(socket.handshake.query);
   console.log(user);
   console.log(`${user.userName} connected to ${room}`);
   superState[room] = superState[room] || loadStore();
   const store = superState[room];
   socket.join(room);
-  const userConnected = action('USER_CONNECTED', user);
+  const roomSocket = io.to(room);
+  const userConnected = action('RECEIVE_USER', user);
   store.dispatch(userConnected);
-  socket.emit('dispatchAction', userConnected);
+  roomSocket.emit('action', userConnected);
   socket.emit(`hydrateUser:${user.id}`, store.getState());
 
   // Upload new store
   socket.on('upload', (newState) => {
     superState[room] = loadStore(newState);
-    socket.emit('hydrate', newState);
+    roomSocket.emit('hydrate', newState);
   });
 
   socket.on('action', (userAction) => {
     store.dispatch(userAction);
-    socket.emit('action', userAction);
+    roomSocket.emit('action', userAction);
   });
 
   // Handle disconnect
   socket.on('disconnect', () => {
-    console.log(`${user} disconnected from ${room}`);
+    console.log(`${user.userName} disconnected from ${room}`);
     const userDisconnected = action('USER_DISCONNECTED', user);
     store.dispatch(userDisconnected);
-    socket.emit('dispatchAction', userDisconnected);
+    roomSocket.emit('action', userDisconnected);
   });
 });
 
--- reducers/users.js
+++ reducers/users.js
@@ -1,7 +1,12 @@
-const usersReducer = (users = {}, action) => {
-  switch (action.type) {
+const usersReducer = (users = {}, { type, payload }) => {
+  switch (type) {
     case 'RECEIVE_USER':
-      return { ...users, [action.user.id]: action.user };
+      return { ...users, [payload.id]: payload };
+    case 'REMOVE_USER': {
+      const newUsers = { ...users };
+      delete users[payload.id];
+      return newUsers;
+    }
     default:
       return users;
   }
